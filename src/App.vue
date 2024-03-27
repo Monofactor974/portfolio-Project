ff422cee0783e0113df0bb3f6e36f389e4683500
Monofactor <boyer.jluc974@gmail.com>
2024-03-27 16:00:23 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Hello world</h1>
+    <MainCard />
+  </div>
+</template>
+
+<script setup>
+import MainCard from "./components/MainCard.vue";
+// Pas besoin de "export default" ici
+
+const name = "App"; // Vous pouvez également déclarer des variables ici
+</script>
+
+<style scoped></style>
---
+++ src/components/MainCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Test portfolio</h1>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Ceci est un exemple de message dans un composant Vue.js.",
+    };
+  },
+};
+</script>
+
+<style scoped>
+/* Ajoutez vos styles CSS ici */
+</style>
